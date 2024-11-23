96651a7a391778feb2a77a402b5a1eb4f0c1df16
Mariano Z. <marianoz@posteo.net>
2024-11-23 10:24:42 -0300

    chore: use remove selfupdate logic

--- cmd/update.go
+++ cmd/update.go
@@ -23,36 +23,23 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"archive/tar"
-	"compress/gzip"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"runtime"
 	"strings"
 
-	"github.com/Masterminds/semver/v3"
-	"github.com/marianozunino/rop/internal/logger"
-	"github.com/minio/selfupdate"
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/afero"
+	"github.com/marianozunino/selfupdater"
 	"github.com/spf13/cobra"
 )
 
 func NewUpdateCmd() *cobra.Command {
-	cfg := &config{}
-
 	updateCmd := &cobra.Command{
 		Use:   "update",
-		Short: "Update rop to the latest version",
+		Short: "Update the rop tool to the latest available version.",
+		Long: `Automatically update the rop tool to the latest available version from the official source.
+This command checks for updates, downloads the new version, and replaces the current executable with the updated one.`,
+		Example: `rop update`,
 		Run: func(cmd *cobra.Command, args []string) {
-			logger.ConfigureLogger(cfg.verbose)
-			if err := runSelfUpdate(http.DefaultClient, afero.NewOsFs(), os.Args[0]); err != nil {
-				fmt.Printf("Error: %v\n", err)
-				os.Exit(1)
-			}
+			currentVersionStr := strings.TrimPrefix(Version, "v")
+			updater := selfupdater.NewUpdater("marianozunino", "rop", "rop", currentVersionStr)
+			updater.Update()
 		},
 	}
 
@@ -62,152 +49,3 @@ func NewUpdateCmd() *cobra.Command {
 func init() {
 	rootCmd.AddCommand(NewUpdateCmd())
 }
-
-func getAssetName() string {
-	os := runtime.GOOS
-	arch := runtime.GOARCH
-
-	switch os {
-	case "darwin":
-		os = "Darwin"
-	case "linux":
-		os = "Linux"
-	}
-
-	switch arch {
-	case "amd64":
-		arch = "x86_64"
-	case "386":
-		arch = "i386"
-
-	}
-
-	return fmt.Sprintf("rop_%s_%s.tar.gz", os, arch)
-}
-
-func runSelfUpdate(httpClient *http.Client, fs afero.Fs, executablePath string) error {
-	log.Info().Msg("Checking for updates...")
-
-	releaseURL := "https://github.com/marianozunino/rop/releases/latest"
-
-	resp, err := httpClient.Get(releaseURL)
-	if err != nil {
-		return fmt.Errorf("error checking for updates: %v", err)
-	}
-	defer resp.Body.Close()
-
-	latestVersionStr := filepath.Base(resp.Request.URL.Path)
-	latestVersionStr = strings.TrimPrefix(latestVersionStr, "v")
-	currentVersionStr := strings.TrimPrefix(Version, "v")
-
-	latestVersion, err := semver.NewVersion(latestVersionStr)
-	if err != nil {
-		return fmt.Errorf("error parsing latest version: %v", err)
-	}
-
-	currentVersion, err := semver.NewVersion(currentVersionStr)
-	if err != nil {
-		return fmt.Errorf("error parsing current version: %v", err)
-	}
-
-	if !latestVersion.GreaterThan(currentVersion) {
-		log.Info().Msg("Current version is the latest")
-		return nil
-	}
-
-	log.Info().Msgf("New version available: %s (current: %s)", latestVersion, currentVersion)
-
-	assetName := getAssetName()
-	downloadURL := fmt.Sprintf("https://github.com/marianozunino/rop/releases/download/v%s/%s", latestVersion, assetName)
-
-	log.Debug().Msgf("Downloading %s from %s", assetName, downloadURL)
-	resp, err = httpClient.Get(downloadURL)
-	if err != nil {
-		return fmt.Errorf("error downloading update: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Extract the tar.gz file
-	tmpDir, err := os.MkdirTemp("", "rop-update")
-	if err != nil {
-		return fmt.Errorf("error creating temp directory: %v", err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	log.Info().Msg("Extracting update package...")
-	if err := extractTarGz(resp.Body, tmpDir); err != nil {
-		return fmt.Errorf("error extracting update: %v", err)
-	}
-
-	// Locate the new binary in the extracted files
-	newBinaryPath := filepath.Join(tmpDir, "rop")
-
-	// Apply the update
-	newBinary, err := os.Open(newBinaryPath)
-	if err != nil {
-		return fmt.Errorf("error opening new binary: %v", err)
-	}
-	defer newBinary.Close()
-
-	log.Info().Msg("Applying update...")
-	err = selfupdate.Apply(newBinary, selfupdate.Options{})
-	if err != nil {
-		if rerr := selfupdate.RollbackError(err); rerr != nil {
-			return fmt.Errorf("failed to rollback from bad update: %v", rerr)
-		}
-		return fmt.Errorf("error updating binary: %v", err)
-	}
-
-	log.Info().Msgf("Successfully updated to version %s", latestVersion)
-	return nil
-}
-
-// extractTarGz extracts a tar.gz file to a specified directory
-
-func extractTarGz(r io.Reader, dst string) error {
-	gz, err := gzip.NewReader(r)
-	if err != nil {
-		return fmt.Errorf("error creating gzip reader: %v", err)
-	}
-	defer gz.Close()
-
-	tr := tar.NewReader(gz)
-	for {
-		header, err := tr.Next()
-		if err == io.EOF {
-			break // End of archive
-		}
-		if err != nil {
-			return fmt.Errorf("error reading tar header: %v", err)
-		}
-
-		target := filepath.Join(dst, header.Name)
-
-		// Ensure the directory exists
-		if header.Typeflag == tar.TypeDir {
-			if err := os.MkdirAll(target, os.FileMode(header.Mode)); err != nil {
-				return fmt.Errorf("error creating directory %s: %v", target, err)
-			}
-			continue
-		}
-
-		// Create the necessary directories
-		if err := os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
-			return fmt.Errorf("error creating directory %s: %v", filepath.Dir(target), err)
-		}
-
-		// Create a new file
-		outFile, err := os.OpenFile(target, os.O_CREATE|os.O_WRONLY, os.FileMode(header.Mode))
-		if err != nil {
-			return fmt.Errorf("error creating file %s: %v", target, err)
-		}
-
-		// Write the file content
-		if _, err := io.Copy(outFile, tr); err != nil {
-			return fmt.Errorf("error writing to file %s: %v", target, err)
-		}
-		outFile.Close()
-	}
-	return nil
-}
-
--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module github.com/marianozunino/rop
 go 1.23.0
 
 require (
-	github.com/Masterminds/semver/v3 v3.3.0
 	github.com/charmbracelet/huh v0.6.0
 	github.com/charmbracelet/lipgloss v0.13.0
-	github.com/minio/selfupdate v0.6.0
-	github.com/spf13/afero v1.11.0
+	github.com/marianozunino/selfupdater v1.0.1
 	github.com/spf13/cobra v1.8.1
 	k8s.io/api v0.31.1
 	k8s.io/apimachinery v0.31.1
@@ -26,9 +24,12 @@ require (
 	github.com/charmbracelet/x/term v0.2.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/google/go-github/v66 v66.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/minio/selfupdate v0.6.0 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
